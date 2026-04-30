9e8f2983f453554af7351f3fb7f375fa707606c9
TTanKDDal <aresby09@gmail.com>
2026-04-30 11:39:15 +0900

    26.04.30-2

--- abib/js/script.js
+++ abib/js/script.js
@@ -1,8 +1,8 @@
 $(function(){
     $('.btn-wrap').mouseenter(function(){
-        $(this).children('.nav-bar').stop().slideDown();
+        $(this).children('.nav-bar').stop().slideDown(300);
     }).mouseleave(function(){
-        $(this).children('.nav-bar').stop().slideUp();
+        $(this).children('.nav-bar').stop().slideUp(300);
     });
 
 });
